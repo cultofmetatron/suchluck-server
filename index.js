d68ded01779ed2f778bed3b2f533e61e16c0db4d
Isis <isiswenger@gmail.com>
2014-07-26 20:10:09 -0700

    add jwt :w

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var app = express();
-var dotenv = require('dotenv');
+var express    = require('express');
+var app        = express();
+var dotenv     = require('dotenv');
+var secret     = 'meowmeowmeowmeowmeowmeow';
+var bodyParser = require('body-parser');
+var jwt 			 = require('jwt-simple');
+var jwtExp		 = require('express-jwt');
 dotenv.load();
 
 
@@ -18,12 +22,41 @@ var users = {
 
 var games = {};
 
+app.post('/login', bodyParser.json() ,function (req, res){
+	console.log(req.body);
+	if (users[req.body.user] && (users[req.body.user] === req.body.password)){
+		var token = jwt.encode( { 'user' :req.body.user }, secret);
+		res.send({token: token});
+	} else {
+		res.send(403, { error:'User not found!' });
+	}
+});
 
+
+
+//create a jwt (jason web token) --> send that to user object using response
+//find how to sign your own docs
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+/**
+take request.body.user and request.body.password and create a token and send it back to the user
+send it back using request.send
+
+express jwt is middleware to take the jwt from the request and intercept it and
+extract user id out of it and put it on request.user so you can access user by request.user.user 
+(first user created by jwt 2nd user is set in user object)
+**/
+
+//bodyparser == middleware to convert to json and puts on req.body so you may access it
 
 //handler set to '/' route
 app.get('/', function (req, res){
 	res.send('Hello Chicken');
 });
 
+
 //tells server to listen on port 3000
+//nothing runs until the listener dispatches it
 app.listen(3000);
