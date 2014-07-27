402d3cc3e33f947a875277f56bf7e6930c873fa9
Isis <isiswenger@gmail.com>
2014-07-26 17:49:55 -0700

    YOLO

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-
+//handler set to '/' route
 app.get('/', function (req, res){
 	res.send('Hello Chicken');
 });
-
+//tells server to listen on port 3000
 app.listen(3000);
